a501a4abcc4bd7378ab92ba09f48db2114d07104
Boluwatife Ajao <ajaoboluwatife2018@gmail.com>
2023-08-10 14:32:28 -0700

    Best style updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <unistd.h> 
+#include <unistd.h>
+
 /**
  * main - Entry point
  * Return: always 1 (success)
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stderr);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }

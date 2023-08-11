7c0c45f3dce2ec09411c3b503abf4d9cfd1563ce
Boluwatife Ajao <ajaoboluwatife2018@gmail.com>
2023-08-11 06:31:15 -0700

    Error debugged

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
  *
- * Return: always returns 0
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    int i;
-    int j;
+	int ones;
+	int tens;
 
-    for (i = 10; i <= 19; i++)
-    {
-        for (j = 10; j <= 19; j++)
-        {
-            putchar((i % 10) + '0');
-            putchar((j % 10) + '0');
-            if (j != 19) 
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return 0;
+	for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
+	{
+		for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
+		{
+			putchar(tens);
+			putchar(ones);
+
+			if (tens != '8' || ones != '9') /*print commas*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
-

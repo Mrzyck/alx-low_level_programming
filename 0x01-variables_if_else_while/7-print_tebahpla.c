6cf084963c3d4ee60f766d8beaf03dd67e3ab5c9
Boluwatife Ajao <ajaoboluwatife2018@gmail.com>
2023-08-11 05:55:26 -0700

    Committed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a' ; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}

f4a6bc369b45054f8c3f01054dfe77ac99522961
Boluwatife Ajao <ajaoboluwatife2018@gmail.com>
2023-08-11 06:26:00 -0700

    corrected code

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  *
- * Return: alway returns 0
- *
+ * Return: always returns 0
  */
 int main(void)
 {
-	int i;
-	int j;
+    int i;
+    int j;
 
-	for (i = 10; i <= 19; i++)
-	{
-		for (j = 10; j <= 19; j++)
-		{
-			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
-			if (i != 19 ||  j != 19)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-
-		}
-	}
-	putchar('\n');
-	return (0);
+    for (i = 10; i <= 19; i++)
+    {
+        for (j = 10; j <= 19; j++)
+        {
+            putchar((i % 10) + '0');
+            putchar((j % 10) + '0');
+            if (j != 19) 
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
+    return 0;
 }
+

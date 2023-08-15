5ea72849327dfb014daf6c8462ed0f396c7e54bd
Boluwatife Ajao <ajaoboluwatife2018@gmail.com>
2023-08-15 05:42:44 -0700

    betty style corrected

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-void print_alphabet(void)
-{
-	char ch;
 
-	for (ch = 97; ch <= 122; ch++)
-		_putchar(ch);
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
 	_putchar('\n');
 }

17e3d2f78e93ac537b0a4834ff04db946ccecca6
Boluwatife Ajao <ajaoboluwatife2018@gmail.com>
2023-08-16 03:57:59 -0700

    Betty style added

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,21 +9,22 @@
  */
 
 int largest_number(int a, int b, int c)
+
 {
-    int largest;
 
-    if (a >= b && a >= c)
-    {
-       largest = a;
-    }
-    else if (b >= a && b >= c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+	int largest;
 
-    return (largest);
+	if (a >= b && a >= c)
+	{
+	largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+	largest = b;
+	}
+	else
+	{
+	largest = c;
+	}
+	return (largest);
 }

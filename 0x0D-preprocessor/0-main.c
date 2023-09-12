f8314c693e5b7c490281502982111992d249084f
Mrzyck <inuayeblog@gmail.com>
2023-09-11 23:11:18 -0700

    betty style fixed

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    int s;
+int s;
 
-    s = 98 + SIZE;
-    printf("%d\n", s);
-    return (0);
+s = 98 + SIZE;
+printf("%d\n", s);
+return (0);
 }

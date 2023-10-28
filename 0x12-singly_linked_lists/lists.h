b60d9a25307ea2b4c0c9f6213a69c8c2d2effd93
Mrzyck <inuayeblog@gmail.com>
2023-10-28 03:22:49 -0700

    betty style added

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,13 +2,13 @@
 #define LISTS_H
 #include <stddef.h>
 /**
- *  * struct list_s - singly linked list
- *   * @str: string - (malloc'ed string)
- *    * @len: length of the string
- *     * @next: points to the next node
- *      *
- *       * Description: singly linked list node structure
- *        */
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;

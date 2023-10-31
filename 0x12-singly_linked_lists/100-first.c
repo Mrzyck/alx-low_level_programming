a726f37c69dceb5cb1c08f33607f9956e09ddb74
Mrzyck <inuayeblog@gmail.com>
2023-10-31 05:51:26 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_before_main - a function that prints a statement before the
+ * main function is executed
+ * Return: Success
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
